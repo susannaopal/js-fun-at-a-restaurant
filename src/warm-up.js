1fb10453cf08b455f332ff2dc577a12b54332579
Susanna Carey <susannaopal@gmail.com>
2021-08-25 17:58:11 -0400

    Initial commit

--- src/warm-up.js
+++ src/warm-up.js
@@ -9,14 +9,15 @@
 // Initialize three new variables, "food", "type" and "inStock", and assign values to them with
 // a food's name, type and number of items in stock.
 
-// var food = "Ice Cream";
-// var type = "Dessert";
-// var inStock = 10
+var food = "Ice Cream";
+var type = "Dessert";
+var inStock = 10
 
 
 
 // Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
 // the inStock count double what it currently is.
+
 
 
 // Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
