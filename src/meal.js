31d12272e94712df43af31ae336a8ce6adfc82ea
Susanna Carey <susannaopal@gmail.com>
2021-08-25 22:11:16 -0400

    Update meals js tests with js

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,26 @@
-function nameMenuItem() {
+function nameMenuItem(foodName) {
+  return `Delicious ${foodName}`
+};
 
+function createMenuItem(name, price, type) {
+  var menuItem = {name: name, price: price, type: type}
+  return menuItem
+}
+
+function addIngredients(foodItem, items) {
+
+  // var ingredients = ingredients.push(foodItem);
+  // // var ingredients = [];
+  // for (var i = 0; i < ingredients.length; i++) {
+  //   ingredients.push(cheese)
+  // }
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
