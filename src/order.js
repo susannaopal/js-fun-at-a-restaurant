beda0f1fc94887a5c914630ee39ee22b1b67fa29
Susanna Carey <susannaopal@gmail.com>
2021-08-26 12:42:46 -0400

    Comeplete listItems tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,30 @@
+function takeOrder(orders, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+  deliveryOrders.push(orders)
+  }
+};
+
+function refundOrder(num, deliveryOrders){
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (num === deliveryOrders[i].orderNumber){
+      deliveryOrders.splice(i, 1)
+    }
+  }
+};
+
+function listItems(orders) {
+  var deliveryOrders = [];
+  for (var i = 0; i < orders.length; i++){
+    deliveryOrders.push(orders[i].item);
+  }
+  return deliveryOrders.join(', ');
+};
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
 }
