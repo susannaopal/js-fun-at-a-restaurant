cda9304dc13d55fcf70bdf70536c46eaae43847e
Susanna Carey <susannaopal@gmail.com>
2021-08-27 18:39:19 -0400

    Complete part of restaurant tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,22 @@
+function createRestaurant(name) {
+return {
+  name: name,
+  menus: {
+    breakfast: [],
+    lunch: [],
+    dinner: [],
+
+  }
+}
+};
+
+function addMenuItem(restaurantName, menuItem) {
+  restaurantName.menus[menuItem.type].push(menuItem)
+};
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem
 }
