c6f1fec106cc2142d3982fdef268f181f01924b7
Susanna Carey <susannaopal@gmail.com>
2021-08-28 19:00:00 -0400

    Add updates to test files

--- src/restaurant.js
+++ src/restaurant.js
@@ -6,21 +6,30 @@ return {
     lunch: [],
     dinner: [],
   }
-}
+  }
 };
 
 function addMenuItem(restaurantName, menuItem){
   for (var i = 0; i < restaurantName.menus[menuItem.type].length; i++) {
-    if (restaurantName.menus[menuItem.type][i] === menuItem) {
+    if (menuItem === restaurantName.menus[menuItem.type][i]) {
       return;
     }
-  }
+}
 return restaurantName.menus[menuItem.type].push(menuItem);
 };
 
+function removeMenuItem(restaurantName, menuItem, type) {
+  for (var i = 0; i < restaurantName.menus[type].length; i++) {
+      if (menuItem === restaurantName.menus[type][i].menuItem) {
+         restaurantName.menus[menuItem].splice(i, 1);
+          return `No one is eating our ${menuItem}- it has been removed from the ${restaurantName.type} menu!`
+      }
+  }
+  return `Sorry, we don't sell ${menuItem}, try adding a new recipe!`
+};
 
 module.exports = {
   createRestaurant,
   addMenuItem,
-  // removeMenuItem
+  removeMenuItem
 }
